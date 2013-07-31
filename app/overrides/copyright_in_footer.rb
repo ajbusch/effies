5fbedbfe72ba16a21902abab5e802ac6737a4b8c
Anthony Busch <anthony.j.busch@gmail.com>
2013-07-31 12:28:09 -0400

    added external buttons to footer

--- app/overrides/copyright_in_footer.rb
+++ app/overrides/copyright_in_footer.rb
@@ -1,5 +1,18 @@
 Deface::Override.new(:virtual_path => "spree/shared/_footer",
-:name => "replace_spree_copyright",
+:name => "add_external_inks",
 :replace => "div#footer-left p",
-:text => "Copyright effiesfairtradestore.com",
+:text => %q+
+Copyright effiesfairtradestore.com
+<a href="http://facebook.com/effiesfairtrade"><img src="/assets/store/facebook.png" alt="Visit us on Facebook"/></a>
+<a href="http://pinterest.com/effiesfairtrade"><img src="/assets/store/pinterest.png" alt="Visit us on Pinterest"/></a>
+<a href="http://twitter.com/effiesfairtrade"><img src="/assets/store/twitter.png" alt="Visit us on twitter"/></a>
++,
+:disabled => false)
+
+Deface::Override.new(:virtual_path => "spree/shared/_footer",
+:name => "replace_spree_copyright",
+:replace => "div#footer-right p",
+:text => %q+
+Copyright effiesfairtradestore.com
++,
 :disabled => false)
